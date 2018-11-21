57bef7d5a653e1882665bffc2c970ba1513511dc
Herman <1Herman.Esterhuizen@gmail.com>
2018-11-22 01:46:06 +0200

    Added Unit Tests

---
+++ CopyDatabaseTests/BooleanToVisibilityConverterTests.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Globalization;
+using System.Windows;
+using DataBaseCompare.Converters;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CopyDatabaseTests {
+    [TestClass]
+    public class BooleanToVisibilityConverterTests {
+        private BooleanToVisibilityConverter converter;
+
+        public BooleanToVisibilityConverterTests() {
+            this.converter = new BooleanToVisibilityConverter();
+        }
+
+        [TestMethod]
+        public void MustBeVisible() {
+            Assert.AreEqual(Visibility.Visible, converter.Convert(true, null, null, CultureInfo.InvariantCulture));
+        }
+
+        [TestMethod]
+        public void MustBeVisibleReverse() {
+            converter.TriggerValue = true;
+            Assert.AreEqual(Visibility.Visible, converter.Convert(false, null, null, CultureInfo.InvariantCulture));
+            converter.TriggerValue = false;
+        }
+
+        [TestMethod]
+        public void MustBeCollapsed() {
+            Assert.AreEqual(Visibility.Collapsed, converter.Convert(false, null, null, CultureInfo.InvariantCulture));
+        }
+
+        [TestMethod]
+        public void MustBeCollapsedReverse() {
+            converter.TriggerValue = true;
+            Assert.AreEqual(Visibility.Collapsed, converter.Convert(true, null, null, CultureInfo.InvariantCulture));
+            converter.TriggerValue = false;
+        }
+
+        [TestMethod]
+        public void MustBeHidden() {
+            converter.IsHidden = true;
+            Assert.AreEqual(Visibility.Hidden, converter.Convert(false, null, null, CultureInfo.InvariantCulture));
+            converter.IsHidden = false;
+        }
+
+        [TestMethod]
+        public void MustBeHiddenReverse() {
+            converter.IsHidden = true;
+            converter.TriggerValue = true;
+            Assert.AreEqual(Visibility.Hidden, converter.Convert(true, null, null, CultureInfo.InvariantCulture));
+            converter.IsHidden = false;
+            converter.TriggerValue = false;
+        }
+    }
+}
